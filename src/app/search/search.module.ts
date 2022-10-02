9432fe550827243a69c3a76ae8887f788967c3fc
Alex Kochnev <madjok87@icloud.com>
2022-10-02 21:40:59 +0300

    feat:add h1

--- src/app/search/search.module.ts
+++ src/app/search/search.module.ts
@@ -3,15 +3,8 @@ import { CommonModule } from '@angular/common';
 import { SearchResultsComponent } from './search-results/search-results.component';
 import { SearchItemComponent } from './search-item/search-item.component';
 
-
-
 @NgModule({
-  declarations: [
-    SearchResultsComponent,
-    SearchItemComponent
-  ],
-  imports: [
-    CommonModule
-  ]
+  declarations: [SearchResultsComponent, SearchItemComponent],
+  imports: [CommonModule],
 })
-export class SearchModule { }
+export class SearchModule {}
